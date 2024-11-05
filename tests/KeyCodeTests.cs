8cd5dedfb588af4e3eda3eb86e5ea318ceef8eeb
Andy Glass <andyglasses@gmail.com>
2024-11-05 14:37:36 +0000

    - Added more constructor method to key code creator (which is renamed from exteensions). - Added common validation codes

--- tests/KeyCodeTests.cs
+++ tests/KeyCodeTests.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using System.Collections;
 
 namespace CodedVector.Response.Tests;
 
@@ -27,7 +28,7 @@ public class KeyCodeTests
     var number = 1;
 
     // Act
-    var result = KeyCodeExtensions.KeyCodeWithNumberDetail(key, code, number);
+    var result = KeyCodeCreator.KeyCodeWithNumberDetail(key, code, number);
 
     // Assert
     result.Key.Should().Be(key);
@@ -43,9 +44,130 @@ public class KeyCodeTests
     var number = 1;
 
     // Act
-    var act = () => KeyCodeExtensions.KeyCodeWithNumberDetail(key, code, number);
+    var act = () => KeyCodeCreator.KeyCodeWithNumberDetail(key, code, number);
 
     // Assert
     act.Should().Throw<ArgumentException>().WithMessage("Code cannot contain a colon");
   }
+
+  [Test]
+  public void KeyCodeWithStringDetail_ShouldReturnKeyCodeWithString()
+  {
+    // Arrange
+    var key = "key";
+    var code = "code";
+    var detail = "detail";
+
+    // Act
+    var result = KeyCodeCreator.KeyCodeWithStringDetail(key, code, detail);
+
+    // Assert
+    result.Key.Should().Be(key);
+    result.Code.Should().Be($"{code}:{detail}");
+  }
+
+  [Test]
+  public void KeyCodeWithStringDetail_ShouldReturnKeyCodeWithoutDetail_WhenDetailIsNullOrWhiteSpace()
+  {
+    // Arrange
+    var key = "key";
+    var code = "code";
+    var detail = string.Empty;
+
+    // Act
+    var result = KeyCodeCreator.KeyCodeWithStringDetail(key, code, detail);
+
+    // Assert
+    result.Key.Should().Be(key);
+    result.Code.Should().Be(code);
+  }
+
+  [Test]
+  public void KeyCodeWithStringDetail_ShouldThrowArgumentException_WhenCodeContainsColon()
+  {
+    // Arrange
+    var key = "key";
+    var code = "cod:e";
+    var detail = "detail";
+
+    // Act
+    var act = () => KeyCodeCreator.KeyCodeWithStringDetail(key, code, detail);
+
+    // Assert
+    act.Should().Throw<ArgumentException>();
+  }
+
+  [Test]
+  public void KeyCodeWithStringDetail_ShouldThrowArgumentException_WhenDetailContainsColon()
+  {
+    // Arrange
+    var key = "key";
+    var code = "code";
+    var detail = "det:ail";
+
+    // Act
+    var act = () => KeyCodeCreator.KeyCodeWithStringDetail(key, code, detail);
+
+    // Assert
+    act.Should().Throw<ArgumentException>();
+  }
+
+  [Test]
+  public void KeyCodeWithStringListDetail_ShouldReturnKeyCodeWithStringList()
+  {
+    // Arrange
+    var key = "key";
+    var code = "code";
+    var details = new List<string> { "detail1", "detail2" };
+
+    // Act
+    var result = KeyCodeCreator.KeyCodeWithStringListDetail(key, code, details);
+
+    // Assert
+    result.Key.Should().Be(key);
+    result.Code.Should().Be($"{code}:{string.Join(";", details)}");
+  }
+
+  [Test]
+  public void KeyCodeWithStringListDetail_ShouldReturnKeyCode_WhenEmptyList()
+  {
+    // Arrange
+    var key = "key";
+    var code = "code";
+    var details = new List<string>();
+
+    // Act
+    var result = KeyCodeCreator.KeyCodeWithStringListDetail(key, code, details);
+
+    // Assert
+    result.Key.Should().Be(key);
+    result.Code.Should().Be(code);
+  }
+
+  [Test]
+  [TestCaseSource(nameof(CodeOrDetailsContainsColonOrSemi_TestCases))]
+  public void KeyCodeWithStringListDetail_ShouldThrowArgumentException_WhenCodeOrDetailsContainsColonOrSemi(string code, string detail1)
+  {
+    // Arrange
+    var key = "key";
+    var details = new List<string> { detail1, "detail2" };
+
+    // Act
+    var act = () => KeyCodeCreator.KeyCodeWithStringListDetail(key, code, details);
+
+    // Assert
+    act.Should().Throw<ArgumentException>();
+  }
+
+  public static IEnumerable CodeOrDetailsContainsColonOrSemi_TestCases
+  {
+    get
+    {
+      yield return new TestCaseData("cod:e", "detail1");
+      yield return new TestCaseData("code", "detai:l1");
+      yield return new TestCaseData("cod;e", "detail1");
+      yield return new TestCaseData("code", "detai;l1");
+    }
+  }
+
 }
